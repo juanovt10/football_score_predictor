b13d7e85823cfb363fc7b40ef9f601081a2f73e5
juanovt10 <>
2023-07-30 11:50:33 +0000

    Install gspread and google-auth to access the google sheet

--- run.py
+++ run.py
@@ -1,4 +1,14 @@
+import gspread
+from google.oauth2.service_account import Credentials
 
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
 
+CREDS = Credentials.from_service_account_file('creds.json')
+SCROPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCROPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('europe_football_leagues')
 
-        
