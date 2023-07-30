a71376ce3516bb313ffd9e569e72279c2058b2af
juanovt10 <>
2023-07-30 13:12:52 +0000

    Set up get home team data and validate data functions that check the list of teams in the premier league and displays feedback if the team is or is not in the premier league

--- run.py
+++ run.py
@@ -12,3 +12,63 @@ SCROPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCROPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('europe_football_leagues')
 
+
+# data = SHEET.worksheet("PremierLeague")
+# original_data = data.get_all_values()
+# transpose_data = [list(row) for row in zip(*original_data)]
+
+# teams = (transpose_data[0])[1:]
+# match_found = False
+
+# team_entry = input("Enter team A here:\n")
+# for team in teams: 
+#     if team_entry == team:
+#         match_found = True
+#         break 
+
+# if match_found:
+#     print('good entry')
+# else: 
+#     print('bad entry')
+
+
+
+
+def get_teamA_data():
+    """
+    Requests the home team to retrive the previous season statistics
+    """
+    while True:
+        print("Please enter the home team")
+        print("Note that this program only assesses the Premier League 2023/24 teams")
+        print("Example: Manchester United")
+
+        team_a = input("Enter team A here:\n")
+
+        if validate_team_entry(team_a):
+            print(f'{team_a} is in the Premier League\n')
+            break
+
+    return team_a
+
+def validate_team_entry(team_entry):
+    """
+    Inside here, it will transpose the columns to rows to have a list will all the 
+    Premier League teams. Then it will compare the value imputted by the user to see 
+    if the team inputted is part of the premier league. 
+    """
+
+    all_data = SHEET.worksheet("PremierLeague")
+    original_data = all_data.get_all_values()
+    transpose_data = [list(row) for row in zip(*original_data)]
+
+    teams = (transpose_data[0])[1:]
+
+    for team in teams:
+        if team_entry == team:
+            return True
+
+    print(f"Sorry but {team_entry} is not in the Premier League\n")
+    return False
+
+get_teamA_data()
